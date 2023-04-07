60a55329d11acbb294578dedeb5e19b41ba40cf7
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-04-07 15:56:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const threejsCanvas = document.querySelector('#threejs-canvas')
 let widht = threejsCanvas.offsetWidht
-let height = threejsCanvas.iffsetHeight
+let height = threejsCanvas.offsetHeight
 
 
 
