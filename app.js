3a3bfd35ce38fdc6af53ca0310a1454938493d00
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-04-08 18:10:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -5,17 +7,25 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const loader = new GLTFLoader();
+
+loader.load( 'Test.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+});
+
+/*
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.rotation.x =  0.5;
 scene.add( cube );
+*/
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.y += 0.01;
+    //cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 
