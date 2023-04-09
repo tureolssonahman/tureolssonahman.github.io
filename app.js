d18df49ba138bfcef7d7a195a652a78e6778b854
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-04-09 17:59:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
 /*
 const loader = new GLTFLoader();
 
 loader.load( 'Test.glb', function ( gltf ) {
 	scene.add( gltf.scene );
 });
-
+*/
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.rotation.x =  0.5;
 scene.add( cube );
-*/
+
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-    //cube.rotation.y += 0.01;
+	cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 
