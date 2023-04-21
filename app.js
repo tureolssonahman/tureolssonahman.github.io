607b55f3f31c48a239bd32247ba2d25428655020
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-04-21 10:23:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ loader.load( 'Test.glb', function ( gltf ) {
 });
 
 scene.background = new THREE.Color(0xffffff);
-
+/*
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.rotation.x =  0.5;
 scene.add( cube );
-
+*/
 const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 
