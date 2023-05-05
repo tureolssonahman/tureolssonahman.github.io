aec7fbb6590983e9ab42f52de790480cbe6c525d
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-05-05 09:59:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-//import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.132.2/examples/js/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
@@ -10,10 +9,9 @@ document.body.appendChild( renderer.domElement );
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
 camera.position.set(-32.5, 15, 190);
 
-//const controls = new OrbitControls( camera, renderer.domElement );
-
 const loader = new GLTFLoader();
 loader.load( 'Teknikkorridoren_v2.glb', function ( gltf ) {
+	loader.scene.roation.y = 0.5;
 	scene.add( gltf.scene );
 });
 
@@ -26,9 +24,7 @@ let counter = -20;
 function animate() {
 	counter++;
 	requestAnimationFrame( animate );
-	camera.position.x += Math.sin(counter*0.01)*0.05;
-	camera.position.y -= Math.sin(counter*0.02)*0.05;
-//	controls.update();
+
 	renderer.render( scene, camera );
 }
 
