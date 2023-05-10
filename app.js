c7c28c6a5897dfcf767d989f2611b8c4ceba3433
Ture Olsson-Åhman <90572968+tureolssonahman@users.noreply.github.com>
2023-05-10 21:22:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ document.body.appendChild( renderer.domElement );
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
 
-camera.position.set(-25, 25, -100);
-camera.lookAt(-25, 0, 0)
+camera.position.x = 0;
+camera.position.y = 30;
+camera.position.z = 75;
+camera.lookAt(-22.5, 0, 75)
+
 
 let model = null;
 
@@ -20,30 +23,30 @@ loader.load( 'Teknikkorridoren_v2.glb', function (gltf) {
 	scene.add( model );
 });
 
-const light = new THREE.PointLight(0xffffff, 1.5, 0);
-light.position.set(-25, 100, -200);
-scene.add( light )
+const light1 = new THREE.PointLight(0xffffff, 0.7, 0);
+light1.position.set(-22.5, 350, 250);
+scene.add( light1 )
 
+const light2 = new THREE.PointLight(0xffffff, 0.3, 0);
+light2.position.set(-22.5, 350, -100);
+scene.add( light2 )
+
+/*
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 49) {
 	camera.position.set(-25, 25, -100);
-	camera.lookAt(-25, 0, 0)
-	light.position.set(-25, 100, -200);
     }
-	else if (keyCode == 50){
-	camera.position.set(-25, 25, 200);
-	camera.lookAt(-25, 0, 125)
-	light.position.set(-25, 100, 200);
-	}
 };
+*/
 
 let counter = 0;
 function animate() {
 	counter += 0.01;
 	requestAnimationFrame( animate );
-	model.rotation.y = Math.sin(counter*0.5)*0.5;
+	camera.position.z = Math.sin(counter/2)*150+50;
+	camera.lookAt(-22.5, 0, 75)
 	renderer.render( scene, camera );
 }
 
